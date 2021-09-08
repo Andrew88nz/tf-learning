60944cce837b0c0703e7fa6b27acc07e3f824cdf
Andrew Burt <andrewb@localhost.localdomain>
2021-09-08 22:03:04 +1200

    Updated

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ resource "aws_instance" "example" {
   ami = "ami-0210560cedcb09f07"
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.instance.id]
+
   user_data = <<-EOF
     #!/bin/bash
     echo "Hello, World" > index.html
@@ -17,11 +18,16 @@ resource "aws_instance" "example" {
 resource "aws_security_group" "instance" {
     name = "terraform-upandrunning-instance"
 
-    ingress =  {
-      cidr_blocks = [ "0.0.0.0/0" ]
+    ingress {
+      cidr_blocks = ["0.0.0.0/0"]
       description = "HTTP Inbound"
       from_port = 8080
       protocol = "tcp"
       to_port = 8080
     }
+}
+
+output "public_ip" {
+  value       = aws_instance.example.public_ip
+  description = "The public IP of the Instance"
 }
